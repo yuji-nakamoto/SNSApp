c24fbe78cfda4d46f7dae2711ce83ef480bf4157
yuji-nakamoto <yuji0411@icloud.com>
2020-02-27 05:06:33 +0900

    Initial Commit

---
+++ SNSApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SNSApp
+//
+//  Created by yuji_nakamoto on 2020/02/27.
+//  Copyright © 2020 yuji_nakamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
