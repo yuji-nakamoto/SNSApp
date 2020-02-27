94cdab731e7a2a710c6f7ccdc26415f3b7212f91
yuji-nakamoto <yuji0411@icloud.com>
2020-02-27 14:30:44 +0900

    PostVC作成開始

---
+++ SNSApp/Controller/PostViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PostViewController.swift
+//  SNSApp
+//
+//  Created by yuji_nakamoto on 2020/02/27.
+//  Copyright © 2020 yuji_nakamoto. All rights reserved.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+
+
+}
